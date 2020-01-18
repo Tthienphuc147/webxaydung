437cee6f1007bdb770158634d1cfa6f840943710
nghiadh2016 <47706371+nghiadh2016@users.noreply.github.com>
2020-01-18 16:10:58 +0700

    update session for admin tab

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,27 +7,41 @@ namespace App\Http\Controllers;
 class AdminController extends Controller
 {
    public function showDashboard(){
+      if(Request()->session()->has('id')){
 
           return view('admin.mainPage.mainPage');
+      }
+      return redirect('/showlogin');
 
     }
 
     /*admin-contact*/
     public function showContactList(){
-          $contact='App\Contact'::where('status',0)->orderby('id','desc')->paginate(6);
-          $contacts='App\Contact'::where('status',1)->orderby('id','desc')->paginate(6);
-          return view('admin.mainPage.contact.contact')->with('contactList',$contact)->with('contactListAccept',$contacts);
+          if(Request()->session()->has('id')){
+            $contact='App\Contact'::where('status',0)->orderby('id','desc')->paginate(6);
+            $contacts='App\Contact'::where('status',1)->orderby('id','desc')->paginate(6);
+            return view('admin.mainPage.contact.contact')->with('contactList',$contact)->with('contactListAccept',$contacts);
+          }
+          return redirect('/showlogin');
+          
     }
      public function showContactDetail($unsigned_name){
+      if(Request()->session()->has('id')){
           $contact='App\Contact'::where('id',$unsigned_name)->first();
           return view('admin.mainPage.contact.detailContact')->with('contactDetail',$contact);
+      }
+      return redirect('/showlogin');
     }
     public function changeContactDetailStatus($unsigned_name){
-      $contact='App\Contact'::find($unsigned_name);
-      $contact->status=1;
-      $contact->save();
-      return view('admin.mainPage.contact.detailContact')->with('contactDetail',$contact);
-}
+      if(Request()->session()->has('id')){
+            $contact='App\Contact'::find($unsigned_name);
+            $contact->status=1;
+            $contact->save();
+            return view('admin.mainPage.contact.detailContact')->with('contactDetail',$contact);
+            }
+      return redirect('/showlogin');
+
+      }
 
         
 }

39e0eab9ac917c64826f7847c18faa8ff97ec897
phuc <Ngoc Phuc@DESKTOP-VPNO4HN>
2020-02-14 16:52:08 +0700

    update

--- resources/views/mainPage/introduction.blade.php
+++ resources/views/mainPage/introduction.blade.php
@@ -10,7 +10,7 @@ Giới thiệu
     <section class="banner_area" style="background: url(../public/img/bgg.jpg); background-size: cover">
         <div class="container">
             <div class="banner_inner_text">
-                <h4>Giới thiệu</h4>
+                <h4><a href="/">Trang chủ</a> / <a href="/introduction">Giới thiệu</a></h4>
 
             </div>
         </div>
